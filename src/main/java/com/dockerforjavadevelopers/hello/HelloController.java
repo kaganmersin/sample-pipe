75098737d845636027c80c6877e3b2c55992d4ca
kaganmersin <46555755+kaganmersin@users.noreply.github.com>
2020-12-29 16:48:08 +0300

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Hello Hepsiburada from @Kagan";
+        return "Hello from @Kagan";
     }
     
 }
